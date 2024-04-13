8eef01b3ef111192f10ea3bc1b28f55f1f7634d2
Sanciprian <diegomtzsanci@gmail.com>
2024-04-13 05:27:17 -0600

    Calibration

--- Raspi/main.py
+++ Raspi/main.py
@@ -1,3 +1,9 @@
-# import sys
-# sys.path.append("../")
-from vision.color_detection import ColorDetection
+import cv2 as cv 
+
+def testDevice(source):
+   cap = cv.VideoCapture(source) 
+   if cap is None or not cap.isOpened():
+       print('Warning: unable to open video source: ', source)
+
+testDevice(0) # no printout
+testDevice(1) # prints message
--- vision/auto_calibration.py
+++ vision/auto_calibration.py
@@ -9,7 +9,7 @@ def nothing(x):
     pass 
 
 # Screen capture
-camara_index = Constants.camara_index
+camara_index = 0   
 cap = cv2.VideoCapture(camara_index)
 cap.set(3,1280)
 cap.set(4,720)
