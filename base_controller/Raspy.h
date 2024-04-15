5e29ff28c54305da0822b30409c13dccb26c4004
Sanciprian <diegomtzsanci@gmail.com>
2024-04-15 16:01:58 -0600

    Fix communication error

--- base_controller/Raspy.h
+++ base_controller/Raspy.h
@@ -5,12 +5,13 @@
 #include "Arduino.h"
 class Raspy{
   public: 
-    Raspy(BNO *bno, LineSensor *line, ColorSensor *color, Movement *robot, Gripper *gripper);
+    Raspy();
     void executeCommand(uint8_t packet_size, uint8_t command, uint8_t* buffer);
     void writeSerial(bool success, uint8_t* payload, int elements);
     void readSerial();
     String flag;
     uint8_t cube_offset;
+    String state();
   
   private:
     BNO *_bno;
